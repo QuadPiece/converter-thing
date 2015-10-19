421b00d1fdc1f1693ea3e9c509bcfdff69404022
Dominik V. Salonen <domen178@gmail.com>
2015-10-19 11:11:26 +0200

    Initial vommit

---
+++ README.md
@@ -0,0 +1,23 @@
+# some-file-converter-script-thing
+
+**This is not a serious piece of code and I mostly uploaded it here just to share/archive it. If you wish to improve it. Please make a fork instead of sending pull requests, I'm too god damn lazy to maintain this thing**
+
+Does exactly what it sounds like.  
+It transforms everything that's not MP3 into V0 MP3s using magic (AKA ffmpeg)
+
+This was made because my phone is shit at handling anything that's not MP3. (fkn lumia phones)
+
+It kinda works. i luv u ffmpeg, u mah bae
+
+## Requirements
+
+* ffmpeg
+* lol dats it
+
+## Usage
+
+* Keep all the files you want to convert in a "Music" folder
+* Do the `./convert.sh` thing
+* Look in the brand new "output" folder
+* ???
+* PROFIT
---
+++ convert.sh
@@ -0,0 +1,40 @@
+shopt -s globstar;
+# Loop through ffmpeg once per extension bc im dumb and this was the laziest way to do it.
+
+echo -e "--- \e[96mConverting FLAC files\e[0m ---"
+
+for track in Music/{,**/}*.flac ; do
+  mkdir -p "output/$(dirname "${track}")"
+  echo -e "Converting \e[92m${track}\e[0m"
+  ffmpeg -i "$track" -c:a mp3 -q:a 0 -threads 4 -loglevel quiet -y "output/$track.mp3"
+done
+
+echo -e "--- \e[96mConverting M4A files\e[0m ---"
+
+for track in Music/{,**/}*.m4a ; do
+  mkdir -p "output/$(dirname "${track}")"
+  echo -e "Converting \e[92m${track}\e[0m"
+  ffmpeg -i "$track" -c:a mp3 -q:a 0 -threads 4 -loglevel quiet -y "output/$track.mp3"
+done
+
+echo -e "--- \e[96mMoving MP3 files\e[0m ---"
+
+for track in Music/{,**/}*.mp3 ; do
+  mkdir -p "output/$(dirname "${track}")"
+  # echo -e "Moving ${track}"
+  cp "$track" "output/$track" > /dev/null
+done
+
+echo -e "--- \e[96mMoving covers\e[0m ---"
+
+for cover in Music/{,**/}cover.png ; do
+  mkdir -p "output/$(dirname "${cover}")"
+  # echo -e "Moving ${cover}"
+  cp "$cover" "output/$cover" > /dev/null
+done
+
+for cover in Music/{,**/}cover.jpg ; do
+  mkdir -p "output/$(dirname "${cover}")"
+  # echo -e "Moving ${cover}"
+  cp "$cover" "output/$cover" > /dev/null
+done
